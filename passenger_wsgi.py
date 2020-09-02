e11aa42d81cd699f9f89ee2d47a48c90a6258beb
Jag_k <30597878+jag-k@users.noreply.github.com>
2020-09-02 19:13:22 +0300

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,4 @@ sys.path.insert(0, '/var/www/u1137116/data/www/domashniypsiholog.ru/')
 sys.path.insert(1, '/var/www/u1137116/data/www/domashniypsiholog.ru/.venv/lib/python3.7/site-packages/')
 
 from main import app
-application = app.wsgi({
-
-})
+application = app
