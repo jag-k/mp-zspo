e1ac6b68e9845187c269909147423bdc80c41a92
Сева же, ну <36499717+onewhosaynope@users.noreply.github.com>
2020-09-21 21:09:07 +0300

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-# import os, sys
-# sys.path.insert(0, '/var/www/u1137116/data/www/domashniypsiholog.ru/')
-# sys.path.insert(1, '/var/www/u1137116/data/www/domashniypsiholog.ru/.venv/lib/python3.7/site-packages/')
+import os, sys
+sys.path.insert(0, '/var/www/u1137116/data/www/domashniypsiholog.ru/')
+sys.path.insert(1, '/var/www/u1137116/data/www/domashniypsiholog.ru/.venv/lib/python3.7/site-packages/')
 
-# from main import app
-# application = app
+from main import app
+application = app
 
 
-import sys
-import os
+# import sys
+# import os
 
-INTERP = os.path.expanduser("/var/www/u1137116/data/flaskenv/bin/python")
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
+# INTERP = os.path.expanduser("/var/www/u1137116/data/flaskenv/bin/python")
+# if sys.executable != INTERP:
+#     os.execl(INTERP, INTERP, *sys.argv)
     
-sys.path.append(os.getcwd())
+# sys.path.append(os.getcwd())
 
-from main import app as application
+# from main import app as application
